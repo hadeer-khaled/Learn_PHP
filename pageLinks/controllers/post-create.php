ec4803f4f5cacb04616064fe31db4f188f863e28
Hadeer Khaled <Hadeer_khaled@ETAX.COM.EG>
2024-07-16 09:46:35 +0300

    validate form

--- pageLinks/controllers/post-create.php
+++ pageLinks/controllers/post-create.php
@@ -1,23 +1,27 @@
 <?php
+$heading = "Create a Post";
+require '../helper_functions.php';
+require './../Database.php';
 
-    $heading = "Create a Post";
-    require '../helper_functions.php';
-    require './../Database.php';
+$errors = [];
 
-
-
-    if($_SERVER['REQUEST_METHOD'] === "POST"){
- 
-        $config  = require('./../config.php');
-        $db = new Database($config );
-        $db->query("insert into posts (title , content , user_id) values (:title , :content , :user_id)" , [
-            ":title" =>$_POST['title'],
-            ":content" =>$_POST['content'],
-            ":user_id"=> 1
+if ($_SERVER['REQUEST_METHOD'] === "POST") {
+    if (strlen($_POST['title']) === 0) {
+        $errors['title'] = "Title is required";
+    }
+    if (strlen($_POST['content']) === 0) {
+        $errors['content'] = "Content is required";
+    }
+    if (empty($errors)) {
+        $config = require('./../config.php');
+        $db = new Database($config);
+        $db->query("INSERT INTO posts (title, content, user_id) VALUES (:title, :content, :user_id)", [
+            ":title" => $_POST['title'],
+            ":content" => $_POST['content'],
+            ":user_id" => 1
         ]);
     }
+}
 
-
-
-require '../views/post-create.view.php' ;
+require '../views/post-create.view.php';
 ?>
--- pageLinks/views/post-create.view.php
+++ pageLinks/views/post-create.view.php
@@ -23,13 +23,35 @@
     <main>
         <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
                 <form method = "POST" action ="">
-                    <label for="title">Title</label>
-                    <input  id="title" name = "title"/>
+                    
 
-                    <label for="content">Content</label>
-                    <textarea id="content" name = "content"></textarea>
+                    <div>
+                        <label for="title" class="block text-sm font-medium leading-6 text-gray-900">Title</label>
+                        <div class="relative mt-2 rounded-md shadow-sm">
+                            <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                                  <!-- <span class="text-gray-500 sm:text-sm">$</span> -->
+                            </div>
+                            <input type="text" name="title" id="title" class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                        </div>
+                        <?php if(isset($errors['title'])): ?>
+                             <p class="text-red-500 text-sm"> <?= $errors['title'] ?>  </p>
+                        <?php endif; ?>
+                    </div>
 
-                    <button class="btn">Add</button>
+                    <div>
+                        <label for="content" class="block text-sm font-medium leading-6 text-gray-900">Content</label>
+                        <div class="relative mt-2 rounded-md shadow-sm">
+                            <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                                  <!-- <span class="text-gray-500 sm:text-sm">$</span> -->
+                            </div>
+                            <textarea type="text" name="content" id="content" class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+                        </div>
+                        <?php if(isset($errors['content'])): ?>
+                             <p class="text-red-500 text-sm"> <?= $errors['content'] ?>  </p>
+                        <?php endif; ?>
+                    </div>
+
+                    <button class="btn btn-danger mt-3">Add</button>
                 </form>    
         </div>
     </main>
